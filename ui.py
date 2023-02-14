d3b7e1c32b552c4ba64e16ed88cd4745704c2dfa
NehaBardeDUKE <110474064+NehaBardeDUKE@users.noreply.github.com>
2023-02-14 15:16:34 -0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -33,6 +33,7 @@ def launch(model_path):
         class_names = ['artificial','human']
         transform=T.Compose([   
             T.Resize(256),
+            T.CenterCrop(224),
             T.ToTensor(),
             T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
